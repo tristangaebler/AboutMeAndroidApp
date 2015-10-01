9da60e2e948fc7ca85214ecdb3321e85f2bf3bfc
tristangaebler <Tristan.Gaebler@gmail.com>
2015-10-01 09:52:11 -0600

    Added pictures and functionality
    Added a big if statement to change facts.

--- app/src/main/java/activity/ctec/aboutmeandroidapp/AboutMeActivity.java
+++ app/src/main/java/activity/ctec/aboutmeandroidapp/AboutMeActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.graphics.Color;
 import android.view.View;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class AboutMeActivity extends AppCompatActivity {
 
@@ -16,6 +19,12 @@ public class AboutMeActivity extends AppCompatActivity {
     private Button startButton;
     private RelativeLayout backgroundLayout;
     private TextView firstFact;
+    private TextView secondFact;
+    private TextView thirdFact;
+    private TextView fourthFact;
+    private TextView fifthFact;
+    private int clickCount;
+    private ImageView book;
 
 
     @Override
@@ -27,6 +36,11 @@ public class AboutMeActivity extends AppCompatActivity {
         startButton = (Button) findViewById(R.id.startButton);
         backgroundLayout = (RelativeLayout) findViewById(R.id.backgroundLayout);
         firstFact = (TextView) findViewById(R.id.firstFact);
+        secondFact = (TextView) findViewById(R.id.secondFact);
+        thirdFact = (TextView) findViewById(R.id.thirdFact);
+        fourthFact = (TextView) findViewById(R.id.fourthFact);
+        fifthFact = (TextView) findViewById(R.id.fifthFact);
+        clickCount = 0;
 
         //Helper method -- Should be private
         setUpListeners();
@@ -67,21 +81,49 @@ public class AboutMeActivity extends AppCompatActivity {
         backgroundLayout.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
     }
 
+    //Changing the text visibility method
+    private void changeVisibility() {
+        if(clickCount == 0) {
+            firstFact.setVisibility(View.VISIBLE);
+            secondFact.setVisibility(View.GONE);
+            clickCount++;
+        }else if(clickCount == 1) {
+            firstFact.setVisibility(View.GONE);
+            secondFact.setVisibility(View.VISIBLE);
+            clickCount++;
+        }else if(clickCount == 2) {
+            firstFact.setVisibility(View.GONE);
+            secondFact.setVisibility(View.GONE);
+            thirdFact.setVisibility(View.VISIBLE);
+            clickCount++;
+        }else if(clickCount == 3) {
+            firstFact.setVisibility(View.GONE);
+            secondFact.setVisibility(View.GONE);
+            thirdFact.setVisibility(View.GONE);
+            fourthFact.setVisibility(View.VISIBLE);
+            clickCount++;
+        }else {
+            firstFact.setVisibility(View.GONE);
+            secondFact.setVisibility(View.GONE);
+            thirdFact.setVisibility(View.GONE);
+            fourthFact.setVisibility(View.GONE);
+            fifthFact.setVisibility(View.VISIBLE);
+            clickCount++;
+        }
+
+    }
+
+    private void changePictureVisibility() {
+        book.setImageResource();
+    }
+
     private void setUpListeners(){
         startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View buttonView){
                 //Where the background changes colors
                 changeBackgroundColor();
-                changeFacts();
-
+                changeVisibility();
             }
         });
-    }
-
-    //Changes the acts
-    private void changeFacts() {
-        if (startButton.isActivated() == true) {
-            firstFact.setVisibility(View.VISIBLE);
-        }
     }
 }
